d880f6eb1f9fd280cb90fce24ac01a60f0d89dd6
ashanick <asha.sundararajan@gmail.com>
2023-11-15 20:26:42 +0530

    Day3.3

--- pages/AboutUs/Team.js
+++ pages/AboutUs/Team.js
@@ -5,7 +5,7 @@ function Team() {
     <div>
         <section className="bg-[#092555] h-[10vw] w-[100vw] text-center text-[#f8f8f8] 
             pt-10 px-6 md:px-20 lg:px-40 xl:px-80 pb-10 md:pt-40 md:pb-40">
-            <p className="titlep">The Foundation has brought together a large and diverse group of STEM professionals, all of whom are "Friends of Joy”. </p>
+            <p className="titlep">The Foundation has brought together a large and diverse group of STEM professionals, all of whom are Friends of Joy. </p>
         </section>
         <section className="text-center p-4 md:p-10 lg:p-20 ">
             <h1 className="h1">Foundation Board of Directors</h1>
